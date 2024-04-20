25fb47174a0f19e7d4970530eb8995837f566330
Kumari Surbhi <kumarisurbhi.ctps@gmailcom>
2024-04-20 16:27:55 +0530

    Made some changes

--- Frontend/src/pages/signup/SignUp.jsx
+++ Frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GenderCheckbox from './GenderCheckbox'
+import { Link } from 'react-router-dom'
+import useSignup from '../../hooks/useSignup';
 
 const SignUp = () => {
+
+	const [inputs , setInputs] = useState({
+		fullName:"",
+		username:"",
+		password:"",
+		confirmPassword:"",
+		gender:""
+	});
+
+	 const {loading , signup} = useSignup()
+
+	const handleCheckboxChange = (gender) =>{
+		setInputs({...inputs,gender})
+	}
+
+
+	const handleSubmit = async (e) =>{
+		e.preventDefault();
+		await signup(inputs);
+
+	};
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
         <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -10,56 +34,64 @@ const SignUp = () => {
             <span className='text-blue-500'> ChatApp</span>
             </h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                      <div>
 						<label className='label p-2'>
-							<span className='text-base label-text'>Full Name</span>
+							<span className='text-base label-text text-white'>Full Name</span>
 						</label>
 						<input
 							type='text'
 							placeholder='Your Name'
 							className='w-full input input-bordered h-10'
+							value={inputs.fullName}
+							onChange={(e)=>setInputs({...inputs, fullName: e.target.value})}
 						/>
 					</div>
                     <div>
 						<label className='label p-2'>
-							<span className='text-base label-text'>Username</span>
+							<span className='text-base label-text text-white'>Username</span>
 						</label>
 						<input
 							type='text'
 							placeholder='Enter username'
 							className='w-full input input-bordered h-10'
+							value={inputs.username}
+							onChange={(e)=>setInputs({...inputs, username: e.target.value})}
 						/>
 					</div>
                     <div>
 						<label className='label p-2'>
-							<span className='text-base label-text'>Password</span>
+							<span className='text-base label-text text-white'>Password</span>
 						</label>
 						<input
 							type='password'
 							placeholder='Enter Password'
 							className='w-full input input-bordered h-10'
+							value={inputs.password}
+							onChange={(e)=>setInputs({...inputs, password: e.target.value})}
 						/>
 					</div>
                     <div>
 						<label className='label p-2'>
-							<span className='text-base label-text'>Confirm Password</span>
+							<span className='text-base label-text text-white'>Confirm Password</span>
 						</label>
 						<input
 							type='password'
 							placeholder='Confirm Password'
 							className='w-full input input-bordered h-10'
+							value={inputs.confirmPassword}
+							onChange={(e)=>setInputs({...inputs, confirmPassword: e.target.value})}
 						/>
 					</div>
 
                     {/* GENDER CHECKBOX */}
-                    <GenderCheckbox/>
+                    <GenderCheckbox onCheckboxChange ={handleCheckboxChange} selectedGender ={inputs.gender}/>
 
 
-                    <a href="#" className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>Already have an account</a>
-                    <button className='btn btn-block btn-sm mt-2'>
-                        Sign Up
-                    </button>
+                    <Link to="/login" href="#" className=' text-white text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>Already have an account</Link>
+                    <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+							{loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+						</button>
             </form>
 
         </div>
--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,37 +1,4 @@
-// import Conversation from "../models/conversion.model.js"
-// import Message from "../models/message.model.js"
 
-// export const sendMessage = async (req, res)=>{
-//    try{
-//     const{message} = req.body;
-//     const {id:receiverId} = req.params;
-//     const senderId = re.user._id;
-
-//     let conversation = await Conversation.findOne({
-//       participants: { $all: [senderId, receiverId] },
-//    });
-
-//    if (!conversation) {
-//       conversation = await Conversation.create({
-//          participants: [senderId, receiverId],
-//       });
-//    }
-
-//    const newMessage = new Message({
-//          senderId,
-// 			receiverId,
-// 			message,
-//    });
-
-//    if (newMessage) {
-//       conversation.messages.push(newMessage._id);
-//    }
-
-
-//    res.status(201).json(newMessage);
-
-
-//    }
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 // import { getReceiverSocketId, io } from "../socket/socket.js";
